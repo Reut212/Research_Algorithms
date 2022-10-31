908c38bb7a20ea77b684b6ac0205593d4de1900d
Reut Hadad <reuthadad123@gmail.com>
2022-10-31 22:56:35 +0200

    add reverse function and notes

--- Python Basics/Q2.py
+++ Python Basics/Q2.py
@@ -4,21 +4,32 @@ import networkx as nx
 
 
 def print_path(parent, start, end):
+    # push the last vertex into the requested_path
     requested_path = [end]
+    # keep adding vertices until reaching to first vertex
     while parent[end] != start:
         requested_path.append(parent[end])
         end = parent[end]
+    # push the first vertex into the requested_path
     requested_path.append(start)
-    print('Your path is: ' + str(reversed(requested_path)))
+    # reverse requested_path and print
+    print('Your path is: ' + str(reverse(requested_path)))
     return requested_path[::-1]
+
+
+# reverse requested_path
+def reverse(requested_path):
+    new_lst = requested_path[::-1]
+    return new_lst
+
 
 
 def breadth_first_search(start, end, neighbor_function):
     # initialize queue parent and visited
-    # push the first path into the queue
+    # push the first vertex into the queue
     queue = [start]
     parent = {}
-    # push the first path into visited
+    # push the first vertex into visited
     visited = [start]
 
     while queue:
@@ -59,7 +70,7 @@ def breadth_first_search(start, end, neighbor_function):
 # [(2,1) : [(3,1), (1,1), (2,2), (2,0)]
 # [(2,2)]
 
-
+# function requested
 def four_neighbor_function(node: any) -> list:  # [(0,1) , (-1,0), (0,1), (0,-1)]
     (x, y) = node
     return [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
@@ -71,7 +82,7 @@ def main():
     g1.add_edge(1, 2)
     g1.add_edge(1, 3)
     g1.add_edge(2, 3)
-    print(breadth_first_search(1, 5, g1.neighbors))
+    print(breadth_first_search(1, 3, g1.neighbors))
     # print(breadth_first_search(start=(0, 0), end=(2, 2), neighbor_function=four_neighbor_function))
 
 
