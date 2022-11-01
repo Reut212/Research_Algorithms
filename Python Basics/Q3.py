aa2cd1cadbfaf09dd0edf7fdd0a458ba334a1dd7
Reut Hadad <reuthadad123@gmail.com>
2022-11-01 03:05:09 +0200

    still trying to figure out how to make sort between different types

---
+++ Python Basics/Q3.py
@@ -0,0 +1,29 @@
+# First Question: Deep sort
+def print_sorted(inputX):
+    print()
+    print('The type is: ' + str(type(inputX)))
+    if type(inputX) is dict:
+        print('The type is: ' + str(type(inputX)))
+        for i in sorted(inputX.items()):
+            print(i, end=" ")
+            del inputX[i]
+    # elif type(inputX) is tuple:
+    #     sorted_tup = tuple(sorted(inputX))
+    #     print('The type is: ' + str(type(inputX)))
+    #     print(sorted_tup)
+
+
+if __name__ == '__main__':
+    x = {"a": 5, "c": 6, "b": [1, 3, 2, 4]}
+    print_sorted(x)  # prints e.g. {"a":5, "b":[1,2,3,4], "c":6}
+    # print()
+    # y = {'student1': ('bhanu', 10), 'student4': ('uma', 12),
+    #         'student3': ('suma', 11), 'student2': ('ravi', 11),
+    #         'student5': ('gayatri', 9)}
+    # print_sorted(y)
+    # print()
+    # z = {('bhanu', 10): 'student1',
+    #     ('uma', 12): 'student4',
+    #     ('suma', 11): 'student3',
+    #     ('ravi', 11): 'student2',
+    #     ('gayatri', 9): 'student5'}
