186f072f5bf237211031c37869af938dd1a2a9ad
Reut Hadad <reuthadad123@gmail.com>
2022-11-01 03:02:21 +0200

    add notes

--- Python Basics/Q1.py
+++ Python Basics/Q1.py
@@ -1,17 +1,20 @@
 # First Question: Safe Call
 def f(x: int, y: float, z):
+    # sum all types together
     return x + y + z
 
 
 def safe_call(func, x: int, y: float, z: any):
     try:
         print('You chose x = ' + str(x) + ', y = ' + str(y) + ' and z = ' + str(z))
+        # raise an exception if x is not type of integer and y is not type of float
         integer_x = int(x)
         float_y = float(y)
     except ValueError:
         print(
             'Please check your inputs. The first input x = ' + str(x) + ' must be integer and the second input y = ' +
             str(y) + ' must be float.')
+    # after types check perform number sum
     else:
         sum_xyz = func(x, y, z)
         print('Sum of x, y and z is: ' + str(sum_xyz))
