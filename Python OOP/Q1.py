7f8ab1ff9ce89520161d7471d385e708fea39e49
Reut Hadad <reuthadad123@gmail.com>
2022-11-13 00:27:48 +0200

    add a doctest documentation

--- Python OOP/Q1.py
+++ Python OOP/Q1.py
@@ -1,5 +1,15 @@
+import os
 import re
 from time import sleep
+
+"""
+This is an implementation for valid email addresses search.
+
+The example program supplies two function, open_a_text_file(txtFileName) and check(email). For example, 
+>>> open_a_text_file("txt.txt") Valid Emails are:  ['abc-d@mail.com', 'abc.def@mail.com', 'abc@mail.com', 
+'abc_def@mail.com', 'abc.def@mail.cc', 'abc.def@mail-archive.com', 'abc.def@mail.org', 'abc.def@mail.com'] Invalid 
+Emails are:  ['abc-@mail.com', 'abc..def@mail.com', '.abc@mail.com', 'abc#def@mail.com', 'abc.def@mail.c', 
+'abc.def@mail#archive.com', 'abc.def@mail', 'abc.def@mail..com'] """
 
 regex = r'^[a-z0-9]+[\._-]?[a-z0-9]+[@][a-z0-9]+[\._-]?[a-z0-9]+[.]\w{2,}$'
 validEmails = []
@@ -7,9 +17,19 @@ invalidEmails = []
 
 
 def open_a_text_file(txtFileName):
-    # txtFileName = input("Please enter a txt file name: ")
-    # print()
+    """
+    Receive txt file name from user and looking for a valid and invalid email addresses according to known pattern (
+    regex).
+    There is an implementation for inserting a txt file and this function also check if the file is a txt file.
+    I marked it as notes.
+    """
 
+    # txtFileName = input("Please enter a txt file name: ")
+    ext = os.path.splitext(txtFileName)[-1].lower()
+    if ext != '.txt':
+        print(ext, "is an unknown file format.")
+        return
+    # print()
     # print("looking for " + str(txtFileName) + "...")
     # print()
     # sleep(0.5)
@@ -30,6 +50,15 @@ def open_a_text_file(txtFileName):
 
 
 def check(email):
+    """
+    recieve email address and add it to invalid / valid lists
+    For example,
+    >>> check('abc-d@mail.com')
+    Valid Emails: ['abc-@mail.com']
+    >>> check('abc-d@mail.com')
+    Invalid Emails are:  ['abc-@mail.com']
+    """
+
     if email == '/0':
         return
     if re.match(regex, email):
@@ -42,3 +71,4 @@ def check(email):
 
 if __name__ == '__main__':
     open_a_text_file("txt.txt")
+    # open_a_text_file()
