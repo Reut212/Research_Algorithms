1e4c6586264f401b61f449f0ac9887d6f79907ee
Reut Hadad <reuthadad123@gmail.com>
2022-11-13 23:17:48 +0200

    add unittests for Q3

--- Python OOP/unitest.py
+++ Python OOP/unitest.py
@@ -1,5 +1,6 @@
 import unittest
 from Q2 import *
+from Q3 import *
 
 
 class MyTestCase(unittest.TestCase):
@@ -20,10 +21,27 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(sum_numbers(2, 1), 3)
         self.assertEqual(sum_numbers(3, 4), "I already told you that the answer is 7!")
         print()
-    #
-    # def test_q3(self):
-    #
-    #     print()
+
+        print(' -------------------------------- Q3 tests -------------------------------- ')
+
+    def test_q3(self):
+        list1 = List([
+            [[1, 2, 3, 33], [4, 5, 6, 66]],
+            [[7, 8, 9, 99], [10, 11, 12, 122]],
+            [[13, 14, 15, 155], [16, 17, 18, 188]],
+        ])
+        self.assertEqual(list1[0, 1, 3], 66)
+        self.assertEqual(list1[0], [[1, 2, 3, 33], [4, 5, 6, 66]])
+        self.assertEqual(list1[4], None)
+        list1[2] = 3
+        self.assertEqual(list1[0], [[1, 2, 3, 33], [4, 5, 6, 66]])
+        self.assertEqual(list1, [[[1, 2, 3, 33], [4, 5, 6, 66]], [[7, 8, 9, 99], [10, 11, 12, 122]], 3])
+        self.assertEqual(list1[None], None)
+        self.assertEqual(list1[1], [[7, 8, 9, 99], [10, 11, 12, 122]])
+        self.assertEqual(list1, [[[1, 2, 3, 33], [4, 5, 6, 66]], [[7, 8, 9, 99], [10, 11, 12, 122]], 3])
+        del (list1[1])
+        self.assertEqual(list1, [[[1, 2, 3, 33], [4, 5, 6, 66]], 3])
+        print()
 
 
 if __name__ == '__main__':
