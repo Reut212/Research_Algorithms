3e0d0047822a05ff10035d11190441a03f786d45
Reut Hadad <reuthadad123@gmail.com>
2022-11-29 22:17:59 +0200

    add Q1

---
+++ Python Design Patterns/Q1.py
@@ -0,0 +1,24 @@
+import itertools
+
+
+def bounded_subsets(lst, c):
+    subset_list = []
+    if c < 0 or len(lst) == 0:
+        yield []
+        return
+    for i in range(len(lst) + 1):
+        for comb in itertools.combinations(lst, i):
+            if sum(comb) <= c and comb not in subset_list:
+                subset_list.append(comb)
+                yield list(comb)
+            else:
+                break
+
+
+if __name__ == '__main__':
+    for s in bounded_subsets([1, 2, 3], 4):
+        print(s)
+    # for s in bounded_subsets(range(50, 150), 103):
+    #     print(s)
+    # for s in zip(range(5), bounded_subsets(range(100), 1000000000000)):
+    #     print(s)
