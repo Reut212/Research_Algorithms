3f732613d5e6f148d2018ff58d375b11af0bb1ac
Reut Hadad <reuthadad123@gmail.com>
2022-11-30 02:35:23 +0200

    finished Q2 + doctests

--- Python Design Patterns/Q2.py
+++ Python Design Patterns/Q2.py
@@ -8,21 +8,25 @@ def shortest_path(algorithm: Callable, out_type: str, input_data):
     """
         Receive algorithm [BFS/DFS] , shortest path/length and sorce vertex, dest vertex and the graph itself.
         There is an implementation for a shortest_path.
+        Used  the same BFS Algorithm as in EX1
         >>> g1 = nx.Graph()
         >>> g1.add_nodes_from(range(0, 5))
         >>> g1.add_edge(0, 1)
         >>> g1.add_edge(0, 3)
         >>> g1.add_edge(1, 3)
         >>> g1.add_edge(2, 4)
-        >>> g1.add_edge(1, 2)
+        >>> g1.add_edge(3, 2)
         >>> print(shortest_path(dfs, "length", (0, 2, g1)))
         The length is: 3
         >>> print(shortest_path(dfs, "path", (0, 2, g1)))
-        The path is: [0, 1, 2]
+        The path is: [0, 3, 2]
         >>> print(shortest_path(breadth_first_search, "length", (0, 2, g1)))
         The length is: 3
         >>> print(shortest_path(breadth_first_search, "path", (0, 2, g1)))
-        The path is: [0, 1, 2]
+        The path is: [0, 3, 2]
+        >>> print(shortest_path(dfs, "path", (0, 5, g1)))
+        There is no path between 0 and 5
+        The path is: []
     """
     graph = input_data[2]
     if isinstance(input_data, list) or isinstance(input_data, tuple):
@@ -92,20 +96,12 @@ def breadth_first_search(start, end, neighbor_function):
     if end not in parent.keys():
         print('There is no path between ' + str(start) + ' and ' + str(end))
         return []
-        # restore the path between start t end
-    tmp = end
-    path = [end]
-    while parent[tmp] != start:
-        path.append(parent[tmp])
-        tmp = parent[tmp]
-    path.append(start)
-    return path[::-1]
 
 
 def dfs(start, end, neighbor_function):
     visited = []
     stack = []
-    parent = {}
+    path = []
 
     visited.append(start)
     stack.append(start)
@@ -113,32 +109,29 @@ def dfs(start, end, neighbor_function):
     while stack:
         # get the first node from the queue
         node = stack.pop()
-
+        path.append(node)
         # going over all adjacent vertices and add each one to his parent, to the queue and mark as visited
         for adjacent in neighbor_function(node):
-            if adjacent == end:
-                parent[adjacent] = node
-                break
             if adjacent not in visited:
-                parent[adjacent] = node
                 visited.append(adjacent)
                 stack.append(adjacent)
-        # there is available path
-        if end in parent.keys():
-            return print_path(parent, start, end)
+                if adjacent == end:
+                    path.append(end)
+                    return path
     # no path available error
-    if end not in parent.keys():
+    if end not in path:
+        print('There is no path between ' + str(start) + ' and ' + str(end))
         return []
 
 
 if __name__ == '__main__':
     print(doctest.testmod())
-    # graph1 = nx.Graph()
-    # graph1.add_nodes_from(range(0, 5))
-    # graph1.add_edge(0, 1)
-    # graph1.add_edge(0, 3)
-    # graph1.add_edge(1, 3)
-    # graph1.add_edge(2, 4)
-    # graph1.add_edge(1, 2)
-    # print(shortest_path(breadth_first_search, "path", (0, 6, graph1)))
-    # print(shortest_path(dfs, "length", (0, 2, graph1)))
+    # g1 = nx.Graph()
+    # g1.add_nodes_from(range(0, 5))
+    # g1.add_edge(0, 1)
+    # g1.add_edge(0, 3)
+    # g1.add_edge(1, 3)
+    # g1.add_edge(2, 4)
+    # g1.add_edge(3, 2)
+    # print(shortest_path(dfs, "path", (0, 5, g1)))
+    # print(shortest_path(dfs, "path", (0, 2, g1)))
